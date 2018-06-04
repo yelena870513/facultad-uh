8c19d832ce0a49b6398cb2865783f3271e9e3e84
felix <firomerorom4@gmail.com>
2018-06-04 03:41:07 -0400

    Sistema de preguntas

---
+++ src/app/sections/question/question.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import * as uuid from 'uuid';
+import {HeaderService} from '../../services/header.service';
+import * as _ from 'lodash';
+import {DataService} from '../../services/data.service';
+import {TranslateService} from '@ngx-translate/core';
+//noinspection TypeScriptCheckImport
+import {log} from 'util';
+declare var M: any;
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+  questions = [];
+  droppedItems = [];
+  page = 1;
+  removeItem(e: any) {
+    this.droppedItems = this.droppedItems.filter((f: any) => f.name !== e.target.innerText);
+  }
+  constructor(private headerService: HeaderService, private dataService: DataService, private translate: TranslateService) {
+    this.headerService.Hide();
+    this.dataService.getQuestion(this.translate.currentLang).subscribe((data: any) => {
+      this.questions = data.docs.sort((a:any, b: any) => a.title - b.title).filter((f) => f.type === 'dragdrop');
+    }, ( error1 => {
+       console.log(error1);
+    } ));
+  }
+  checkAnswer(item,event) {
+    if(event.target.checked){
+        if(item.value === true){
+            item.resource = 'T';
+        }
+        else{
+          item.resource = 'F';
+        }
+    }
+    else{
+      item.resource = 'N';
+    }
+  }
+  onItemDrop(e: any, answer) {
+    // Get the dropped data here
+    //noinspection TypeScriptUnresolvedFunction
+    if(_.isUndefined(answer.dropped)){
+      answer.dropped = [];
+    }
+
+    if(answer.dropped.length < answer.values.length && !_.find(answer.dropped, (value) => value === e.dragData)){
+    answer.dropped.push(e.dragData);
+    }
+  }
+
+  ngOnInit() {
+
+  }
+}
